d97da75c708a357b23aafb24f75e160ec7668b6c
Filipee01 <99333746+Filipee01@users.noreply.github.com>
2022-05-21 23:59:08 -0300

    .

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
     li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    alert(li.children[0].href)
   }
 }
 
