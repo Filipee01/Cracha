f7f5abfa70d8a408f5200eaf3f5c1e2711346379
Filipee01 <99333746+Filipee01@users.noreply.github.com>
2022-05-21 23:19:53 -0300

    Crachar/ API  Filipe nlw heat

---
+++ main.js
@@ -0,0 +1,17 @@
+function showMeSomething() {
+  alert(links.instagram)
+}
+
+const links = {
+  github: 'Filipee01',
+  youtube: 'GameOverYoutuber',
+  facebook: 'FilipeCFAL',
+  instagram: 'filipee.01',
+  twitter: 'Filipeee01'
+}
+
+function changeSocialMediaLinks() {
+  document.getElementById('userName').textContent = 'Filipe'
+}
+
+changeSocialMediaLinks()
