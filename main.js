f8eb06ee881ee07975060f29298700e1c9813ebe
Filipee01 <99333746+Filipee01@users.noreply.github.com>
2022-06-01 11:04:32 -0300

    js

--- main.js
+++ main.js
@@ -14,8 +14,9 @@ function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
     li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
-    alert(li.children[0].href)
   }
 }
 
 changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {}
